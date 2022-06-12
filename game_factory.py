abb2daf3c4103d79aa5a6eb11f85eb96fad49473
yordans-petrovs <107316416+yordans-petrovs@users.noreply.github.com>
2022-06-12 17:06:41 +0300

    Update game_factory.py

--- game_factory.py
+++ game_factory.py
@@ -33,31 +33,3 @@ class Game:
             return 1
         else:
             return 0
-
-
-# team_names = [
-#     "ACM", "Internazionale", "Juventus", "Roma", "Lazio", "Torino", "Bologna", "Cagliari", "Monza",
-#     "Ternana", "Fiorentina", "Crotone", "Napoli", "Cremonese", "Peruggia", "Spezia", "Atalanta", "Como"
-# ]
-# teams_cities = [
-#     "Milan", "Milan", "Torino", "Rome", "Rome", "Torino", "Bologna", "Cagliari", "Monza", "Ternana",
-#     "Firenze", "Crotone", "Neapol", "Cremona", "Peruggia", "Spezia", "Bergamo", "Como"
-# ]
-# teams_budgets = [107, 10, 20, 110, 15, 89, 85, 45, 61, 92, 103, 56, 11, 8, 21, 35, 14, 15]
-#
-# c = Tournament("Serie A", "Italy", 18)
-# country = "Italy"
-#
-# for i in range(18):
-#     t = TeamFactory(team_names[i], teams_cities[i], country, teams_budgets[i])
-#     c.add_team(t)
-# print(c.get_table())
-#
-# g = Game(c, "ACM", "Internazionale", 2, 0, 0, 1, 0, 0)
-# print(g.tournament.name)
-# c.get_data_from_game(g)
-# print(c.get_table())
-# g = Game(c, "Roma", "Internazionale", 2, 0, 0, 1, 0, 0)
-# print(g.tournament.name)
-# c.get_data_from_game(g)
-# print(c.get_table())
